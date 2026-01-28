55f4d65f8b683c0088aeb461fd72426e7cd57542
Stefan Deshev <stefandeshev@gmail.com>
2026-01-28 09:12:25 +0200

    Progress on home page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import 'zone.js';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject } from '@angular/core';
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroComponent, HeaderComponent, ContentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  readonly el = inject(ElementRef);
+
+  readonly observer = new IntersectionObserver((entries) =>
+    console.info(entries),
+  );
+
+  ngAfterViewInit(): void {
+    this.observer.observe(this.el.nativeElement);
+  }
+}
